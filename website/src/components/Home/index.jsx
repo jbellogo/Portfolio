0f01e23086a3b91dbb13f1c05e0bd31349c58539
Juan Bello <jbnermal3@gmail.com>
2025-01-21 19:19:30 -0500

    personal website with react

---
+++ website/src/components/Home/index.jsx
@@ -0,0 +1,15 @@
+import AboutMe from "../AboutMe";
+import Footer from "../Footer";
+import HeroSection from "../HeroSection";
+import MyPortfolio from "../MyPortfolio";
+
+export default function Home() {
+  return (
+    <>
+      <HeroSection />
+      <AboutMe />
+      <MyPortfolio />
+      <Footer />
+    </>
+  );
+}
